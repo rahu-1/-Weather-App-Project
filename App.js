92422d29aabbede4c8a96739ecb05be51ba274c5
rahu-1 <96769732+rahu-1@users.noreply.github.com>
2022-08-01 22:58:39 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// import UseEffect from './component/Hooks/useEffect'
+//import Todo from './component/todoreact/todo'
+import Temp from './component/weather/temp'
+const App = () => {
+  return <Temp/>
+}
+
+export default App
