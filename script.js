3faea7e05748b224d3908b335417049c159fa052
rahu-1 <96769732+rahu-1@users.noreply.github.com>
2022-12-19 17:19:16 +0530

    Update and rename temp.js to script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+let result=document.getElementById("result");
+let searchBtn=document.getElementById("search-btn");
+let cityRef=document.getElementById("city");
+let getWeather= ()=>{
+    let cityValue=cityRef.value;
+    if(cityValue.length==0)
+    result.innerHTML=`<h3 class="msg">Please enter a city name </h3>`
+    else{
+        let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`
+        cityRef.value="";
+        fetch(url)
+        .then((resp)=>resp.json())//json   == java script object notation convert into object form 
+        //if city name is valid 
+        .then((data)=>{
+    result.innerHTML=
+    `<h2>${data.name}</h2>
+
+    <h4 class="weather">${data.weather[0].main}</h4>
+    <h4 class="desc">${data.weather[0].description}</h4>
+    <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+
+    <h1>${data.main.temp} &#176;</h1>
+    <div class="temp-container">
+    <div>
+    <h4 class="title">min</h4>
+    <h4 class="temp">${data.main.temp_min}&#176;</h4>
+    </div>
+    <div>
+    <h4 class="title">max</h4>
+    <h4 class="temp">${data.main.temp_max}&#176;</h4>
+    </div>
+    
+    </div>
+    `;
+
+        })
+        //if city name is not valid 
+        .catch(()=>{
+            result.innerHTML=`<h3 class="msg">city not found </h3>`;
+        })
+
+    }
+}
+searchBtn.addEventListener("click",getWeather);
+window.addEventListener("load",getWeather);
