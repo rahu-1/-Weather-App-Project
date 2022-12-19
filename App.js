8ea05366f339c6bf6e3e01deea87ff2d508e4f15
rahu-1 <96769732+rahu-1@users.noreply.github.com>
2022-12-19 17:16:29 +0530

    Update and rename App.js to index.html

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-// import UseEffect from './component/Hooks/useEffect'
-//import Todo from './component/todoreact/todo'
-import Temp from './component/weather/temp'
-const App = () => {
-  return <Temp/>
-}
-
-export default App
